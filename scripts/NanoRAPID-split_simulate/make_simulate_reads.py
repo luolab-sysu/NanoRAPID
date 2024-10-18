96216b519fbef6c7c77b8d0d49e57ab9639f810b
rzhbit <luolabservers@126.com>
2024-10-18 14:47:40 +0800

    update

--- scripts/NanoRAPID-split_simulate/make_simulate_reads.py
+++ scripts/NanoRAPID-split_simulate/make_simulate_reads.py
@@ -1,6 +1,6 @@
 import random
 
-def generate_structure(structure_length=400, open_region_length=50):
+def generate_structure(structure_length=370, open_region_length=45):
     region = []
     while len(region) < open_region_length:
         region_length = int(random.random()*10) + 5
@@ -24,7 +24,6 @@ def generate_read(structure, open_regions, read_length=400, mod_ratio=0.10):
     return read
 
 
-
 def generate_reads(num_reads, structure, open_regions, read_length=400, mod_ratio=0.10):
     """
     生成多条reads的序列信息
@@ -35,25 +34,36 @@ def generate_reads(num_reads, structure, open_regions, read_length=400, mod_rati
 structure1_open_regions = generate_structure()
 structure2_open_regions = generate_structure()
 
+#添加公共开放区域
+for i in range(370,400):
+    structure1_open_regions.append(i)
+    structure2_open_regions.append(i)
+
+print(f"#{len(structure1_open_regions)}\t{structure1_open_regions}")
+print(f"#{len(structure2_open_regions)}\t{structure2_open_regions}")
+
 reads_counts = 1000
+read_length = 400
+open_region_length=45
+false_ratio= 0.02
+mod_ratio=0.10
+
 
 # 生成结构1和结构2的reads
 structure1_reads = generate_reads(reads_counts, "Structure1", structure1_open_regions)
 structure2_reads = generate_reads(reads_counts, "Structure2", structure2_open_regions)
 
-print(f"#{structure1_open_regions}")
-print(f"#{structure2_open_regions}")
-positive_ratio= 0.02
+
 for n in range(reads_counts):
     index = f'structure1_read_{n}'
-    for i in range(400):
-        if random.random() < positive_ratio:
+    for i in range(read_length):
+        if random.random() < false_ratio:
             structure1_reads[n][i] = 1
         print(f'{index}\t{i}\t{structure1_reads[n][i]}')
         
 for n in range(reads_counts):
     index = f'structure2_read_{n}'
-    for i in range(400):
-        if random.random() < positive_ratio:
+    for i in range(read_length):
+        if random.random() < false_ratio:
             structure1_reads[n][i] = 1
         print(f'{index}\t{i}\t{structure2_reads[n][i]}')
--- scripts/NanoRAPID_anomaly-detection/from_eventalign_get_rawsignal.py
+++ scripts/NanoRAPID_anomaly-detection/from_eventalign_get_rawsignal.py
@@ -1,164 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
-import re
 import numpy as np
 import time
 
 def nowtime():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-
-def get_embed_index_dict(file):
-    embed_index_dict = {}
-    for line in open(file):
-        lineL = line.strip().split(' ')
-        motif = lineL[-1]
-        index = lineL[1]
-        embed_index_dict[motif] = index
-    return embed_index_dict
-
-def read_process(database,read_name,strand,idx):
-    sites, kmers, signals = database
-    cans = set([sites[i] for i, kmer in enumerate(kmers) if motif.match(kmer)])
-    idx = idx
-    for can in cans:
-        first_index = sites.index(can)
-        last_index = len(sites) - 1 - sites[::-1].index(can)
-        middle_index = (first_index + last_index) // 2
-
-        # +200 -200
-        start_index = max(0, middle_index - 199)
-        end_index = min(len(kmers), middle_index + 201)
-
-        can_motif = kmers[middle_index]
-        kmer_slice = kmers[start_index:end_index]
-        signal_slice = signals[start_index:end_index]
-
-        #save to txt
-        if len(signal_slice) == 400:
-            
-            kmer_slice_array = [motif_dic[i] for i in kmer_slice]
-            seq_out = " ".join(kmer_slice_array)
-            signal_out = "\t".join(signal_slice)
-            extra_out = can+"|"+read_name+"|"+can_motif
-            
-            print(extra_out,file=ext_file)
-            print(seq_out,file=seq_file)
-            print(signal_out,file=sig_file)
-            #print(can,read_name,can_motif,file=extra_file,sep="\t")
-            idx = idx + 1
-    return idx
-
-
-bedfile = sys.argv[1]
-eventalignfile = sys.argv[2]
-output_dir = sys.argv[3]
-sample_name = sys.argv[4]
-
-file="/public/work/zehui/work/public_data_result/NBT_ONT_result/h9_mod_rep1/code/rawsignal/embed.txt"
-motif_dic = get_embed_dict()
-#base to onehot
-encoding_dict = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
-
-strands = {}
-with open(bedfile,"r") as f:
-    for line in f:
-        info = line.strip().split("\t")
-        strands[info[3]] = info[5]
-
-#candidate motif
-#motif = re.compile(r'.[ATG]A[TC].')
-motif = re.compile(r'[ATCG][ATCG][ATCG][ATCG][ATCG]')
-
-
-sig_file = open(output_dir + "/" + sample_name +"_signal.txt","w")
-seq_file = open(output_dir + "/" + sample_name +"_sequence.txt","w")
-ext_file = open(output_dir + "/" + sample_name +"_extra.txt","w")
-#extra_file = open(output_dir + "/" + sample_name +"_extra_info.txt","w")
-
+eventalignfile = sys.argv[1]
+outfile = sys.argv[2]
+reads_num = 0
+pre_read_name = ""
+read_dict = {}
 with open(eventalignfile,"r") as f:
-    
     # header
     next(f)
-    #first line
-    line = f.readline().strip()
-    info = line.strip().split("\t")
-    database = [[],[],[]]
-    read_name = info[3]
-    strand = strands.get(read_name)
-    if strand == "+" and info[9] != "NNNNN":
-        site = info[0]+"|"+info[1]+"|+"
-        ran = int(info[14]) - int(info[13])
-        database[0][:0] = [site] * ran
-        database[1][:0] = [info[9]] * ran
-        database[2][:0] = list(map(float, info[15].split(",")))
-    elif strand == "-" and info[9] != "NNNNN":
-        site = info[0]+"|"+info[1]+"|-"
-        ran = int(info[14]) - int(info[13])
-        database[0].extend([site] * ran)
-        database[1].extend([info[9]] * ran)
-        database[2].extend(map(float, info[15].split(",")))
-    else:
-        pass
-
-    n = 2
-    idx = 0
     for line in f:
-        info = line.strip().split("\t")
-        if info[3] == read_name:
-            strand = strands.get(info[3])
-            if strand == "+" and info[9] != "NNNNN":
-                site = info[0]+"|"+info[1]+"|+"
-                ran = int(info[14]) - int(info[13])
-                database[0][:0] = [site] * ran
-                database[1][:0] = [info[9]] * ran
-                database[2][:0] = list(map(float, info[15].split(",")))
-            elif strand == "-" and info[9] != "NNNNN":
-                site = info[0]+"|"+info[1]+"|-"
-                ran = int(info[14]) - int(info[13])
-                database[0].extend([site] * ran)
-                database[1].extend([info[9]] * ran)
-                database[2].extend(map(float, info[15].split(",")))
-            else:
-                pass
-            
-            n = n+1
-            if n % 500000 == 0:
-                print(f'{nowtime()} | {sample_name} {n} line finish')
-        else:
-            #previous read process
-            strand = strands.get(read_name)
-            idx = read_process(database,read_name,strand,idx)
+        line = line.strip()
+        lineL = line.split("\t")
+        read_name = lineL[3]
+        if read_name != pre_read_name:
+            for (gene_name,site,motif),singal_list in read_dict.items():
+                #outlist = [f"{gene_name}|{site}|{motif}|{pre_read_name}",np.mean(singal_list),np.std(singal_list),np.median(singal_list),len(singal_list)]
+                outlist = [gene_name,site,motif,pre_read_name,np.mean(singal_list),np.std(singal_list),np.median(singal_list),len(singal_list)]
+                print("\t".join([str(n) for n in outlist]),file=open(outfile,"a"))
+                #print("----")
+            #--
+            pre_read_name = read_name
+            read_dict = {}
+            reads_num += 1
+            if reads_num % 1000 == 0:
+                print("%s\t%d"%(nowtime(),reads_num))
+        gene_name = lineL[0]
+        site = int(lineL[1])
+        motif = lineL[2]
+        read_dict.setdefault((gene_name,site,motif),[])
+        singal_list = [float(n) for n in lineL[15].split(",")]
+        read_dict[(gene_name,site,motif)] += singal_list
 
-            database = [[],[],[]]
-            read_name = info[3]
-            strand = strands.get(read_name)
-            if strand == "+" and info[9] != "NNNNN":
-                site = info[0]+"|"+info[1]+"|+"
-                ran = int(info[14]) - int(info[13])
-                database[0][:0] = [site] * ran
-                database[1][:0] = [info[9]] * ran
-                database[2][:0] = list(map(float, info[15].split(",")))
-            elif strand == "-" and info[9] != "NNNNN":
-                site = info[0]+"|"+info[1]+"|-"
-                ran = int(info[14]) - int(info[13])
-                database[0].extend([site] * ran)
-                database[1].extend([info[9]] * ran)
-                database[2].extend(map(float, info[15].split(",")))
-            else:
-                pass
-            
-            n = n+1
-            if n % 500000 == 0:
-                print(f'{nowtime()} | {sample_name} {n} line finish')
-    
-    #last read process
-    strand = strands.get(read_name)
-    idx = read_process(database,read_name,strand,idx)
 
-sig_file.close()
-seq_file.close()
-ext_file.close()
-#extra_file.close()
-
-print(f"{nowtime()} | {sample_name} all finish")
 
