d56db68efd3e8cb5b44add52bfae2e2e0950aa8a
luolab-sysu <luolabservers@126.com>
2025-12-02 10:57:14 +0800

    Update evenalign.sh
    Signed-off-by: luolab-sysu <luolabservers@126.com>

--- scripts/DRS-basecall-mapping/evenalign.sh
+++ scripts/DRS-basecall-mapping/evenalign.sh
@@ -11,7 +11,7 @@ check_program "nanopolish"
 
 wd="$1"
 data="$wd/data"
-fa="/disk2/work/zehui/NBT_ONT_result/Tetra_test/ref/ribosomal_RNA_self-splicing_intron.fa"
+fa="$2"
 # 检查参数是否为空
 if [ -z $wd ] && [ -z $fa ]; then
     echo "missing argvs" # 打印错误信息
